7d6edb7cf67c2c27da2c66e016f34321e2c7c82d
Queency Koh <Queency.Koh@iridium-technology.com>
2021-08-09 20:23:09 +0800

    Added Controllers

---
+++ CleanArchitecture/CleanArchitecture.WebUI/Controllers/TodoListsController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CleanArchitecture.WebUI.Data;
+using CleanArchitecture.WebUI.Models;
+
+namespace CleanArchitecture.WebUI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoListsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TodoListsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TodoLists
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoList>>> GetTodoLists()
+        {
+            return await _context.TodoLists
+                .Select(list => new TodoList
+                {
+                    Id = list.Id,
+                    Title = list.Title,
+                    Items = list.Items.Select(item => new TodoItem
+                    {
+                        Id = item.Id,
+                        List = item.List,
+                        Title = item.Title,
+                        Done = item.Done,
+                        PriorityLevel = item.PriorityLevel,
+                        Note = item.Note
+                    }).ToList()
+                }).ToListAsync();
+        }
+
+        // PUT: api/TodoLists/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoList(int id, TodoList todoList)
+        {
+            if (id != todoList.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todoList).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoListExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TodoLists
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<long>> PostTodoList(TodoList todoList)
+        {
+            _context.TodoLists.Add(todoList);
+            await _context.SaveChangesAsync();
+
+            return todoList.Id;
+        }
+
+        // DELETE: api/TodoLists/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoList(int id)
+        {
+            var todoList = await _context.TodoLists.FindAsync(id);
+            if (todoList == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoLists.Remove(todoList);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TodoListExists(int id)
+        {
+            return _context.TodoLists.Any(e => e.Id == id);
+        }
+    }
+}
