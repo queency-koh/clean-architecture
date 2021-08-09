d2227d1166916b2687daafcf5be0bb90bb64f7a5
Queency Koh <Queency.Koh@iridium-technology.com>
2021-08-09 11:32:10 +0800

    Apply changes to DB

--- CleanArchitecture/CleanArchitecture.WebUI/Data/Migrations/20210808111635_Todos.Designer.cs
+++ CleanArchitecture/CleanArchitecture.WebUI/Data/Migrations/20210808111635_Todos.Designer.cs
@@ -4,14 +4,16 @@ using CleanArchitecture.WebUI.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CleanArchitecture.WebUI.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210808111635_Todos")]
+    partial class Todos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
