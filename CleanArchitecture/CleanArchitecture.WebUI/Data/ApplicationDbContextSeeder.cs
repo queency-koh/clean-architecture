ff703de7595a9c0c5a95aec73e04be89a48cd19a
Queency Koh <Queency.Koh@iridium-technology.com>
2021-08-09 11:32:27 +0800

    Add Data Seeder

---
+++ CleanArchitecture/CleanArchitecture.WebUI/Data/ApplicationDbContextSeeder.cs
@@ -0,0 +1,31 @@
+﻿using CleanArchitecture.WebUI.Models;
+using System.Linq;
+
+namespace CleanArchitecture.WebUI.Data
+{
+    public static class ApplicationDbContextSeeder
+    {
+        public static void Seed(ApplicationDbContext context) 
+        {
+            if (context.TodoLists.Any())
+            {
+                return;
+            }
+
+            var list = new TodoList
+            {
+                Title = "Death List Five",
+                Items =
+                {
+                    new TodoItem { Title = "O-Ren Ishii", Done = true },
+                    new TodoItem { Title = "Vernita Green", Done = true },
+                    new TodoItem { Title = "Budd", Done = true },
+                    new TodoItem { Title = "Bill" }
+                }
+            };
+
+            context.TodoLists.Add(list);
+            context.SaveChanges();
+        }
+    }
+}
